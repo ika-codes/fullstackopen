80256d863fac2f9e0918aacc37dacc1926afe3f8
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2019-10-18 16:23:15 +0200

    Finish Phonebook step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
-const App = props => {
-    const [persons, setPersons] = useState(props.entries);
+const App = () => {
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [newSearch, setNewSearch] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons").then(response => {
+            setPersons(response.data);
+        });
+    }, []);
 
     const addPerson = event => {
         event.preventDefault();
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 
-const entries = [
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-];
-
-ReactDOM.render(<App entries={entries} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
