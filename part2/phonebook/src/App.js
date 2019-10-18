f8d16f85765b89aac3acf9878147bed2f83fcb60
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2019-10-18 14:35:48 +0200

    Finish Phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,21 +4,25 @@ import Entry from "./components/Entry";
 const App = props => {
     const [persons, setPersons] = useState(props.entries);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
 
     const rows = () =>
         persons.map(entry => <Entry key={entry.name} entry={entry} />);
 
     const addPerson = event => {
         event.preventDefault();
-        let checkName = persons.filter(x => x.name === newName);
+        let checkName = persons.filter(
+            x => x.name.toLowerCase() === newName.toLowerCase()
+        );
 
         if (checkName.length === 0) {
             const personObject = {
                 name: newName,
-                number: ""
+                number: newNumber
             };
             setPersons(persons.concat(personObject));
             setNewName("");
+            setNewNumber("");
         } else {
             window.alert(`${newName} is already added to phonebook`);
         }
@@ -26,6 +30,10 @@ const App = props => {
 
     const handleNameChange = event => {
         setNewName(event.target.value);
+    };
+
+    const handleNumberChange = event => {
+        setNewNumber(event.target.value);
     };
 
     return (
@@ -36,12 +44,15 @@ const App = props => {
                     name: <input value={newName} onChange={handleNameChange} />
                 </div>
                 <div>
+                    number:{" "}
+                    <input value={newNumber} onChange={handleNumberChange} />
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             <ul>{rows()}</ul>
-            <div>debug: {newName}</div>
         </div>
     );
 };
