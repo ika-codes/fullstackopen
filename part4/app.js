97acbf9f19ca48c95c4d8efcab877dbe4e825847
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2019-12-08 10:00:36 +0100

    Finish Part 4 Step 2

---
+++ part4/app.js
@@ -0,0 +1,18 @@
+const config = require("./utils/config");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const mongoose = require("mongoose");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
