bc17098f19c8bd2ee03bd9a2cad664dadcb401bf
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2019-12-08 10:49:02 +0100

    Finish Part 4 Unit tests step 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = blogs => {
 	return 1;
 };
 
+const totalLikes = blogs => {
+	const sum = blogs.reduce(
+		(accumulator, currentValue) => accumulator + currentValue.likes,
+		0
+	);
+	return blogs.length === 0 ? 0 : sum;
+};
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 };
