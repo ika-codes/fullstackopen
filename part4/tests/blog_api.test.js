9a037d647ea27c63d359f43b466c6c59e9b136db
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2019-12-21 13:26:56 +0100

    Finish Part 4 Blog list tests step 2

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,36 +2,17 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blog");
+const apiHelper = require("./api_helper");
 
 const api = supertest(app);
-
-const initialBlogs = [
-	{
-		_id: "5a422a851b54a676234d17f7",
-		title: "React patterns",
-		author: "Michael Chan",
-		url: "https://reactpatterns.com/",
-		likes: 7,
-		__v: 0
-	},
-	{
-		_id: "5a422aa71b54a676234d17f8",
-		title: "Go To Statement Considered Harmful",
-		author: "Edsger W. Dijkstra",
-		url:
-			"http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-		likes: 5,
-		__v: 0
-	}
-];
 
 beforeEach(async () => {
 	await Blog.deleteMany({});
 
-	let blogObject = new Blog(initialBlogs[0]);
+	let blogObject = new Blog(apiHelper.initialBlogs[0]);
 	await blogObject.save();
 
-	blogObject = new Blog(initialBlogs[1]);
+	blogObject = new Blog(apiHelper.initialBlogs[1]);
 	await blogObject.save();
 });
 
@@ -42,12 +23,20 @@ test("blogs are returned as json", async () => {
 		.expect("Content-Type", /application\/json/);
 });
 
-test("there are 2 notes", async () => {
+test("there are 2 blogs", async () => {
 	const response = await api.get("/api/blogs");
 
-	expect(response.body.length).toBe(initialBlogs.length);
+	expect(response.body.length).toBe(apiHelper.initialBlogs.length);
 });
 
+test("there is a property under id", async () => {
+	const blogsAtEnd = await apiHelper.blogsInDb();
+
+	const idsInBlogs = blogsAtEnd.map(n => n.id);
+	expect(idsInBlogs).toBeDefined();
+});
+
+/*
 test("a valid note can be added ", async () => {
 	const newBlogPost = {
 		_id: "5a422aa71b54a676234d1573",
@@ -86,7 +75,7 @@ test("blog without content is not added", async () => {
 	const response = await api.get("/api/blogs");
 
 	expect(response.body.length).toBe(initialBlogs.length);
-});
+}); */
 
 afterAll(() => {
 	mongoose.connection.close();
